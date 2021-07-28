71e11617b8dff0592d58008b0b3a0ce7cf408ce5
Lera24 <bedulinavalera21@gmail.com>
2021-07-28 23:14:29 +0300

    HW_13

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import './sass/main.scss';
+import onSearchData from './search_img';
+
+const searchForm = document.querySelector('.search-form');
+const loadMore = document.querySelector('.load-more');
+const selector = 'button-hidden';
+loadMore.classList.add(selector);
+
+searchForm.addEventListener('submit', onSearchApi);
+
+function onSearchApi (e) {
+    e.preventDefault();
+
+    onSearchData.clearMarcup();
+    onSearchData.clearCounter();
+    loadMore.classList.add(selector);
+
+    onSearchData.query(e.target.elements.searchQuery.value);
+    onSearchData.onSearchImg ();
+    loadMore.classList.remove(selector);
+};
+
+
+loadMore.addEventListener('click', showMorePictures);
+
+function showMorePictures (e) {
+    e.preventDefault();
+    onSearchData.counter();
+    onSearchData.onSearchImg ();
+    onSearchData.hiddenBtn(loadMore, selector);
+
+}
---
+++ src/search_img.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+import pictures from './partials/markup.hbs';
+import Notiflix from "notiflix";
+
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const API_KEY = '22659523-ac255ed135817256fe2f96438';
+const API_OPTIONS = 'image_type=photo&orientation=horizontal&safesearch=&true';
+
+const markup = document.querySelector('.gallery');
+
+const errorMessage = 'Sorry, there are no images matching your search query. Please try again.';
+const infoMessage = `We're sorry, but you've reached the end of search results.`;
+
+let resultQuantity = '';
+let valueResponse = '';
+let currentValue = '';
+let PAGE = '';
+
+const PER_PAGE = 40;
+
+
+async function onSearchImg () {
+    const url = `/?key=${API_KEY}&q=${currentValue}&${API_OPTIONS}&page=${PAGE}&per_page=${PER_PAGE}`;
+    const response = await axios.get(url);
+
+    valueResponse = response.data.hits;
+    resultQuantity = response.data.totalHits;
+    createMarkup();
+};
+
+function createMarkup () {
+    if (!valueResponse.length) {
+        fetchErrors();
+    }
+    markup.insertAdjacentHTML('beforeend', pictures ({response : valueResponse}));
+    const successfulMessage = `Hooray! We found ${resultQuantity} images.`;
+    Notiflix.Notify.success(successfulMessage);
+};
+
+const fetchErrors = (() => {
+    return Notiflix.Notify.failure(errorMessage);
+  });
+
+function counter () {
+    return PAGE += 1;
+}
+
+const query = ((value) => {
+    currentValue = value;
+    return currentValue;
+});
+
+function clearMarcup () {
+    return markup.innerHTML = '';
+};
+
+function clearCounter () {
+    return PAGE = Number(1);
+};
+
+function hiddenBtn (elements, property) {
+    if (markup.children.length >= resultQuantity) {
+        Notiflix.Notify.info(infoMessage);
+        return elements.classList.add(property);
+   }
+    return;
+}
+
+export default {onSearchImg, counter, clearCounter, query, clearMarcup, hiddenBtn};
